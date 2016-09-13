0498c0ac818cc2cce3f133d4bd1ca4e18c496997
Ayana Powell and J. Ryan Conklin <ayana@>
2016-09-13 13:47:43 -0700

    Solve for Nickels, Dimes and Quarters.

--- src/CoinCombo.php
+++ src/CoinCombo.php
@@ -3,11 +3,13 @@
     class CoinCombo
     {
         private $pennies;
+        private $nickels;
 
         //Constructor
 
-        function _construct($pennies) {
+        function _construct($pennies, $nickels) {
             $this->pennies = $pennies;
+            $this->nickels = $nickels;
         }
 
         //Getter and Setter
@@ -22,6 +24,16 @@
             return $this->pennies;
         }
 
+        function setNickels($new_nickels)
+        {
+            $this->nickels = $new_nickels;
+        }
+
+        function getNickels()
+        {
+            return $this->pennies;
+        }
+
         //General Methods
 
         function makePennies($input)
@@ -29,7 +41,41 @@
             return $input / .01;
         }
 
+        function makeNickels($input)
+        {
+            $remainder = number_format(fmod($input, .05), 2);
+            $parsed_array = array();
+            $whole = $input / .05;
+            $whole = round($whole - $remainder);
+            array_push($parsed_array, $whole);
+            array_push($parsed_array, $remainder);
 
+            return $parsed_array;
+        }
+
+        function makeDimes($input)
+        {
+            $remainder = number_format(fmod($input, .10), 2);
+            $parsed_array = array();
+            $whole = $input / .10;
+            $whole = round($whole - $remainder);
+            array_push($parsed_array, $whole);
+            array_push($parsed_array, $remainder);
+
+            return $parsed_array;
+        }
+
+        function makeQuarters($input)
+        {
+            $remainder = number_format(fmod($input, .25), 2);
+            $parsed_array = array();
+            $whole = $input / .25;
+            $whole = round($whole - $remainder);
+            array_push($parsed_array, $whole);
+            array_push($parsed_array, $remainder);
+
+            return $parsed_array;
+        }
 
 
 
--- tests/CoinComboTest.php
+++ tests/CoinComboTest.php
@@ -10,5 +10,30 @@
             $this->assertEquals(4, $output);
         }
 
+        function test_makeNickels_float06()
+        {
+            $coin_test = new CoinCombo;
+            $input = .06;
+            $output = $coin_test->makeNickels($input);
+            $this->assertEquals(array(1, .01), $output);
+        }
+
+        function test_makeDimes_float11()
+        {
+            $coin_test = new CoinCombo;
+            $input = .11;
+            $output = $coin_test->makeDimes($input);
+            $this->assertEquals(array(1, .01), $output);
+        }
+
+        function test_makeQuarters_float26()
+        {
+            $coin_test = new CoinCombo;
+            $input = .26;
+            $output = $coin_test->makeQuarters($input);
+            $this->assertEquals(array(1, .01), $output);
+        }
+
+
     }
 ?>
